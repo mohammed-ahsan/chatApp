119bdccfc3f3f748edf19b8145a177f115cc202d
Ahsan <37025966+mohammed-ahsan@users.noreply.github.com>
2022-09-03 23:52:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http")
+const cors = require("cors")
+const { Server } = require("socket.io");
+
+app.use(cors())
+const server = http.createServer(app)
+const io = new Server(server, { cors: {
+    origin: "http://localhost:3000"
+  }});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`user id: ${socket.id} joined room: ${data}`);
+  });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+server.listen(3001,()=>{console.log("server running")})
+
