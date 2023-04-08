fb0f9340c008ade4c2d2ffbbb8a26fffdd3f7db4
Ahsan <37025966+mohammed-ahsan@users.noreply.github.com>
2023-04-08 20:05:26 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://52.77.248.174");
+const socket = io.connect("https://memoe.me");
 
 function App() {
   const [username, setUsername] = useState("");
